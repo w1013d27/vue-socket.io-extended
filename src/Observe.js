f8bd02ad8b365f1f121032825d82651e2d9e4eb8
w1013d27 <w1013d27@126.com>
2022-05-02 17:51:40 +0800

    Update Observe.js

--- src/Observe.js
+++ src/Observe.js
@@ -1,98 +1,80 @@
 import GlobalEmitter from './GlobalEmitter';
-import { getRegisteredMutations, getRegisteredActions, trimNamespace } from './utils/vuex';
+import {getRegisteredMutations, getRegisteredActions, trimNamespace} from './utils/vuex';
 import defaults from './defaults';
-import { SYSTEM_EVENTS } from './constants';
+import {SYSTEM_EVENTS} from './constants';
 import {
-  unwrapIfSingle,
-  prefixWith,
-  pipe,
-  augmentMethod,
+    unwrapIfSingle,
+    prefixWith,
+    pipe,
+    augmentMethod,
 } from './utils';
 
-export default (Socket, { store, ...otherOptions } = {}) => {
-  const options = { ...defaults, ...otherOptions };
+export default (Socket, {store, ...otherOptions} = {}) => {
+    const options = {...defaults, ...otherOptions};
 
-  const eventToAction = pipe(
-    options.eventToActionTransformer,
-    prefixWith(options.actionPrefix),
-  );
+    const eventToAction = pipe(
+        options.eventToActionTransformer,
+        prefixWith(options.actionPrefix),
+    );
 
-  const eventToMutation = pipe(
-    options.eventToMutationTransformer,
-    prefixWith(options.mutationPrefix),
-  );
+    const eventToMutation = pipe(
+        options.eventToMutationTransformer,
+        prefixWith(options.mutationPrefix),
+    );
 
-  function passToStore(event, payload) {
-    if (!store) return;
+    function passToStore(event, payload) {
+        if (!store) return;
 
-    const desiredMutation = eventToMutation(event);
-    const desiredAction = eventToAction(event);
-    const mutations = getRegisteredMutations(store);
-    const actions = getRegisteredActions(store);
-    const unwrappedPayload = unwrapIfSingle(payload);
+        const desiredMutation = eventToMutation(event);
+        const desiredAction = eventToAction(event);
+        const mutations = getRegisteredMutations(store);
+        const actions = getRegisteredActions(store);
+        const unwrappedPayload = unwrapIfSingle(payload);
 
-     mutations
-      .filter((namespacedMutation) => 
-              {
-      
-      if(options?.mutation)
-            {
-                if(options.mutation[event])
-                {
-                    if(options.mutation[event]===namespacedMutation)
-                    {
-                        return true;
-                    }else {
-                        return false;
+        mutations
+            .filter((namespacedMutation) => {
+
+                    if (options.mutation ? options.mutation[event] : undefined) {
+                        return options.mutation[event] === namespacedMutation;
                     }
+
+                    return trimNamespace(namespacedMutation) === desiredMutation;
                 }
-            }
-            return trimNamespace(namespacedMutation) === desiredMutation;
+            )
+            .forEach((namespacedMutation) => store.commit(namespacedMutation, unwrappedPayload));
+
+        actions
+            .filter((namespacedAction) => {
+                    if (options.action ? options.action[event] : undefined) {
+                        return options.action[event] === namespacedAction;
+                    }
+
+                    return trimNamespace(namespacedAction) === desiredAction;
+                }
+            )
+            .forEach((namespacedAction) => store.dispatch(namespacedAction, unwrappedPayload));
     }
-             
-             )
-      .forEach((namespacedMutation) => store.commit(namespacedMutation, unwrappedPayload));
 
-    actions
-      .filter((namespacedAction) => 
-              {  if(options?.action)
-            {
-                if(options.action[event])
-                {
-                    if(options.action[event]===namespacedAction)
-                    {
-                        return true;
-                    }else {
-                        return false;
-                    }
-                }
+    function registerEventHandler() {
+        augmentMethod(Socket, 'onevent', (packet) => {
+            const [eventName, ...args] = packet.data;
+            let mappedEventName = eventName;
+
+            if (otherOptions.eventMapping) {
+                mappedEventName = otherOptions.eventMapping(eventName, args);
             }
-            return trimNamespace(namespacedAction) === desiredAction;}
-             
-             )
-      .forEach((namespacedAction) => store.dispatch(namespacedAction, unwrappedPayload));
-  }
 
-  function registerEventHandler() {
-    augmentMethod(Socket, 'onevent', (packet) => {
-      const [eventName, ...args] = packet.data;
-      let mappedEventName = eventName;
+            GlobalEmitter.emit(mappedEventName, ...args);
+            passToStore(mappedEventName, args);
+        });
 
-      if (otherOptions.eventMapping) {
-        mappedEventName = otherOptions.eventMapping(eventName, args);
-      }
+        SYSTEM_EVENTS.forEach((eventName) => {
+            Socket.on(eventName, (...args) => {
+                GlobalEmitter.emit(eventName, ...args);
+                passToStore(eventName, args);
+            });
+        });
+    }
 
-      GlobalEmitter.emit(mappedEventName, ...args);
-      passToStore(mappedEventName, args);
-    });
-
-    SYSTEM_EVENTS.forEach((eventName) => {
-      Socket.on(eventName, (...args) => {
-        GlobalEmitter.emit(eventName, ...args);
-        passToStore(eventName, args);
-      });
-    });
-  }
-
-  registerEventHandler();
+    registerEventHandler();
 };
